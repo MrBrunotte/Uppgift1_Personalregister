fcd667bdc342188cb230f267a40c52d54539d2a9
Stefan Brunotte <mrbrunotte@gmail.com>
2022-10-25 14:03:53 +0200

    Add project files.

---
+++ Uppgift1_Personalregister/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift1_Personalregister
+{
+    class Employee
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public int Salary { get; set; } = 0;
+
+        public Employee()
+        {
+
+        }
+        public Employee(string firstName, string lastName, int salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+        }
+        public static void AddEmployee(string firstName, string lastName, int salary)
+        {
+            Console.Write("Type first name of employee: ");
+            firstName = Console.ReadLine();
+            Console.Write("Type last name of employee: ");
+            lastName = Console.ReadLine();
+            Console.Write("Type employee salary: ");
+            salary = int.Parse(Console.ReadLine());
+
+            ChangeConsoleColor();
+            Console.WriteLine($"\n\tFirst name: {firstName}\n" +
+                              $"\tLast name: {lastName}\n" +
+                              $"\tSalary: {salary}\n");
+            ResetConsoleColor();
+        }
+        private static void ChangeConsoleColor()
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        private static void ResetConsoleColor()
+        {
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Uppgift1_Personalregister/Program.cs
@@ -0,0 +1,33 @@
+﻿using Uppgift1_Personalregister;
+
+Console.WriteLine("Uppgift 1: Personal register\n" +
+                  "Kriterier:\n" +
+                  "\n1. Registret skall kunna ta emot och lagra anställda med namn och lön\n" +
+                  "2. Programmet skall kunna skriva ut registret i konsolen\n");
+Console.WriteLine("####### Welcome #######\n");
+
+bool exit = true;
+do
+{
+    Console.WriteLine("What would you like to do?\n\n" +
+                      "1) ADD new employee\n" +
+                      "2) PRINT employees\n" +
+                      "3) EXIT");
+
+    int userChoice = int.Parse(Console.ReadLine());
+
+    if (userChoice == 1)
+    {
+        Employee.AddEmployee(firstName: "Stefan", lastName: "Brunotte", salary: 38_000);
+    }
+    else if (userChoice == 2)
+    {
+        Console.WriteLine("Print employee");
+    }
+    else
+    {
+        Environment.Exit(3);
+    }
+} while (exit);
+
+
